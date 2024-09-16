2dae6a17c73ed7113f54b972b386f1068e3969dc
Steve Arnold <starnold@redhat.com>
2024-09-16 18:32:35 +0100

    change

--- rhel-9.4-image-mode/build.sh
+++ rhel-9.4-image-mode/build.sh
@@ -6,6 +6,7 @@ cd ../store-display
 ./mvnw clean
 ./mvnw package -Dmaven.test.skip=true
 cp -R target ../rhel-9.4-image-mode/store-display/
+cp -R src/main/resources/META-INF/resources/index.html ../rhel-9.4-image-mode/store-display/target/app
 
 # now cd into the store camera, build it, and then copy the artefacts back out into the container folder
 cd ../store-camera
