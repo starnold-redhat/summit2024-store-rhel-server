d5f13c00fcacb3aefddbc48bbf7f25f1fadea484
Steve Arnold <starnold@redhat.com>
2024-10-03 12:03:43 +0100

    added agenda.json

--- rhel-9.4-image-mode/build.sh
+++ rhel-9.4-image-mode/build.sh
@@ -6,7 +6,8 @@ cd ../store-display
 ./mvnw clean
 ./mvnw package -Dmaven.test.skip=true
 cp -R target ../rhel-9.4-image-mode/store-display/
-cp -R src/main/resources/META-INF/resources/index.html ../rhel-9.4-image-mode/store-display/target/quarkus-app/app
+cp -R src/main/resources/META-INF/resources/* ../rhel-9.4-image-mode/store-display/target/quarkus-app/app
+cp ./agenda.json ../rhel-9.4-image-mode/store-display
 
 # now cd into the store camera, build it, and then copy the artefacts back out into the container folder
 cd ../store-camera
